f78e0b5a0329897c4bbe291c398402726b7c3918
kaixuan <kaixuanlu1992@163.com>
2019-07-03 16:08:05 +0800

    io

--- src/main/java/structures/BinaryTree.java
+++ src/main/java/structures/BinaryTree.java
@@ -1,104 +1,104 @@
-package structures;
-
-public class BinaryTree<T extends Comparable> {
-    public class Node<T extends Comparable>{
-        T value;
-        Node<T> left;
-        Node<T> right;
-
-        Node(T value,Node<T> left,Node<T>right){
-            this.value=value;
-            this.left=left;
-            this.right=right;
-        }
-    }
-
-    private Node<T> root;
-
-    public Node insert(T key){
-         root=insert(key,root);
-         return root;
-    }
-
-    public Node remove(T key){
-        root=remove(key,root);
-        return root;
-    }
-
-    public void midOrder(){
-        midOrder(root);
-    }
-
-    private void midOrder(Node root){
-        if (root!=null){
-            midOrder(root.left);
-            System.out.println(root.value);
-            midOrder(root.right);
-        }
-    }
-
-    public boolean contain(T key){
-        return contain(key,root)!=null;
-    }
-
-    private Node contain(T key, Node<T> root) {
-        if (root==null){
-            return null;
-        }
-        int compareTo=key.compareTo(root.value);
-        if (compareTo>0){
-            return contain(key,root.right);
-        } else if (compareTo<0){
-            return contain(key,root.left);
-        }else {
-            return root;
-        }
-    }
-
-    private Node<T> insert(T key, Node<T> root){
-        if (root==null){
-            return new Node<>(key,null,null);
-        }
-        int compareRs =key.compareTo(root.value);
-        if (compareRs>0){
-            root.right=insert(key,root.right);
-        }else if(compareRs<0){
-            root.left=insert(key,root.left);
-        }else
-            ;
-        return root;
-    }
-
-    private Node<T> remove(T key, Node<T> root){
-        if (root==null){
-            return root;
-        }
-        int compareRs =key.compareTo(root.value);
-        if (compareRs>0){
-            root.right=remove(key,root.right);
-        }else if (compareRs<0){
-            root.left=remove(key,root.left);
-        }else if (root.left!=null&&root.right!=null){
-            root.value=findMin(root.right).value;
-            root.right=remove(root.value,root.right);
-        }else {
-            root=(root.left==null)?root.right:root.left;
-        }
-        return root;
-    }
-
-    private Node<T> findMin(Node root){
-        if (root==null){
-            return null;
-        }else if (root.left==null){
-            return root;
-        }else {
-            return findMin(root.left);
-        }
-    }
-
-    public Node<T> findMin(){
-      return findMin(root);
-    }
-
-}
+//package structures;
+//
+//public class BinaryTree<T extends Comparable> {
+//    public class Node<T extends Comparable>{
+//        T value;
+//        Node<T> left;
+//        Node<T> right;
+//
+//        Node(T value,Node<T> left,Node<T>right){
+//            this.value=value;
+//            this.left=left;
+//            this.right=right;
+//        }
+//    }
+//
+//    private Node<T> root;
+//
+//    public Node insert(T key){
+//         root=insert(key,root);
+//         return root;
+//    }
+//
+//    public Node remove(T key){
+//        root=remove(key,root);
+//        return root;
+//    }
+//
+//    public void midOrder(){
+//        midOrder(root);
+//    }
+//
+//    private void midOrder(Node root){
+//        if (root!=null){
+//            midOrder(root.left);
+//            System.out.println(root.value);
+//            midOrder(root.right);
+//        }
+//    }
+//
+//    public boolean contain(T key){
+//        return contain(key,root)!=null;
+//    }
+//
+//    private Node contain(T key, Node<T> root) {
+//        if (root==null){
+//            return null;
+//        }
+//        int compareTo=key.compareTo(root.value);
+//        if (compareTo>0){
+//            return contain(key,root.right);
+//        } else if (compareTo<0){
+//            return contain(key,root.left);
+//        }else {
+//            return root;
+//        }
+//    }
+//
+//    private Node<T> insert(T key, Node<T> root){
+//        if (root==null){
+//            return new Node<>(key,null,null);
+//        }
+//        int compareRs =key.compareTo(root.value);
+//        if (compareRs>0){
+//            root.right=insert(key,root.right);
+//        }else if(compareRs<0){
+//            root.left=insert(key,root.left);
+//        }else
+//            ;
+//        return root;
+//    }
+//
+//    private Node<T> remove(T key, Node<T> root){
+//        if (root==null){
+//            return root;
+//        }
+//        int compareRs =key.compareTo(root.value);
+//        if (compareRs>0){
+//            root.right=remove(key,root.right);
+//        }else if (compareRs<0){
+//            root.left=remove(key,root.left);
+//        }else if (root.left!=null&&root.right!=null){
+//            root.value=findMin(root.right).value;
+//            root.right=remove(root.value,root.right);
+//        }else {
+//            root=(root.left==null)?root.right:root.left;
+//        }
+//        return root;
+//    }
+//
+//    private Node<T> findMin(Node root){
+//        if (root==null){
+//            return null;
+//        }else if (root.left==null){
+//            return root;
+//        }else {
+//            return findMin(root.left);
+//        }
+//    }
+//
+//    public Node<T> findMin(){
+//      return findMin(root);
+//    }
+//
+//}
--- src/main/java/structures/Test.java
+++ src/main/java/structures/Test.java
@@ -1,22 +1,22 @@
-package structures;
-
-public class Test {
-    public static void main(String[] args){
-        BinaryTree<Integer> tree=new BinaryTree<>();
-        tree.insert(23);
-        tree.insert(24);
-        tree.insert(44);
-        tree.insert(56);
-        tree.insert(76);
-        tree.insert(12);
-        tree.insert(14);
-        tree.insert(98);
-        tree.insert(3);
-        tree.insert(66);
-        tree.insert(23);
-        tree.midOrder();
-        System.out.println(tree.contain(15));
-        System.out.println(tree.contain(14));
-
-    }
-}
+//package structures;
+//
+//public class Test {
+//    public static void main(String[] args){
+//        BinaryTree<Integer> tree=new BinaryTree<>();
+//        tree.insert(23);
+//        tree.insert(24);
+//        tree.insert(44);
+//        tree.insert(56);
+//        tree.insert(76);
+//        tree.insert(12);
+//        tree.insert(14);
+//        tree.insert(98);
+//        tree.insert(3);
+//        tree.insert(66);
+//        tree.insert(23);
+//        tree.midOrder();
+//        System.out.println(tree.contain(15));
+//        System.out.println(tree.contain(14));
+//
+//    }
+//}
