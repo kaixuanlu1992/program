8e29e9d8b8da8d85a603195b537a9ccb424e3ad4
kaixuan <kaixuanlu1992@163.com>
2019-08-08 16:38:03 +0800

    leetcode

---
+++ src/main/java/com/work/base/leetcode/ListNode.java
@@ -0,0 +1,8 @@
+package com.work.base.leetcode;
+
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
---
+++ src/main/java/com/work/framework/biz/model/User.java
@@ -0,0 +1,11 @@
+package com.work.framework.biz.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Integer id;
+    private String userName;
+    private String phone;
+
+}
