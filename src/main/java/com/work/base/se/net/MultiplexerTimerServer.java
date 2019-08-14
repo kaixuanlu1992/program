b1cf1805a2c1e20426cc940aa74fa8184620eca8
kaixuan <kaixuanlu1992@163.com>
2019-08-14 16:41:18 +0800

    socket

---
+++ src/main/java/com/work/base/se/net/MultiplexerTimerServer.java
@@ -0,0 +1,92 @@
+package com.work.base.se.net;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+public class MultiplexerTimerServer implements Runnable {
+    private Selector selector;
+
+    private ServerSocketChannel serverSocketChannel;
+
+    private volatile boolean stop;
+
+    public MultiplexerTimerServer(int port){
+        try  {
+            selector = Selector.open();
+            serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.configureBlocking(false);
+            serverSocketChannel.socket().bind(new InetSocketAddress(port),1024);
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            System.out.println("the server started in port : " + port);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void stop(){
+        this.stop=true;
+    }
+    @Override
+    public void run() {
+        while (!stop){
+            try{
+                selector.select(100);
+                Set<SelectionKey> selectionKeys=selector.selectedKeys();
+                Iterator<SelectionKey> it=selectionKeys.iterator();
+                SelectionKey key = null;
+                while (it.hasNext()){
+                    key = it.next();
+                    it.remove();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handInput(SelectionKey key) throws IOException {
+        if (key.isValid()){
+            if (key.isAcceptable()){
+                ServerSocketChannel ssc = (ServerSocketChannel) key.channel();
+                SocketChannel sc = ssc.accept();
+                sc.configureBlocking(false);
+                sc.register(selector,SelectionKey.OP_READ);
+            }
+            if (key.isReadable()){
+                SocketChannel sc = (SocketChannel) key.channel();
+                ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+                int readBytes = sc.read(byteBuffer);
+                if (readBytes > 0){
+                    byteBuffer.flip();
+                    byte[] bytes = new byte[byteBuffer.remaining()];
+                    byteBuffer.get(bytes);
+                    String body = new String(bytes, "utf-8");
+
+                } else if (readBytes < 0 ){
+                    key.cancel();
+                    sc.close();
+                } else {
+                    ;
+                }
+            }
+        }
+    }
+
+    private void doWrite(SocketChannel channel, String reponse) throws IOException {
+        if (reponse!=null && reponse.trim().length()>0){
+            byte[] bytes=reponse.getBytes();
+            ByteBuffer byteBuffer = ByteBuffer.allocate(bytes.length);
+            byteBuffer.put(bytes);
+            byteBuffer.flip();
+            channel.write(byteBuffer);
+        }
+
+    }
+}
---
+++ src/main/java/com/work/base/se/net/NIOTimeServer.java
@@ -0,0 +1,9 @@
+package com.work.base.se.net;
+
+public class NIOTimeServer {
+    public static void main(String[] args){
+        int port = 7090;
+
+    }
+
+}
